f3e7f6988e9a3f26849ad4b9acccff354daac120
Davis,Joel T <davis.joel@heb.com>
2020-04-11 10:42:30 -0500

    sure thing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
+import ReactDom from 'react-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div id="app" className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Step 1</h1>
       </header>
     </div>
   );
+}
+
+function Header(value) {
+  return (
+    <h1>{value}</h1>
+  )
+}
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="Button" onClick={this.handleClick}>
+        {this.props.value}
+      </button>
+    );
+  }
+
+  handleClick() {
+    const element = Header("YO")
+    ReactDom.render(element, document.getElementById("instructions"))
+  }
 }
 
 export default App;
